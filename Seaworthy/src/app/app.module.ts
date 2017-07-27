07c625e6b568da2eb69d1b0955dd225375d6a3ce
Angelica Valenta <angelicavalenta@gmail.com>
2017-07-27 11:08:30 +0200

    feat: added map

--- Seaworthy/src/app/app.module.ts
+++ Seaworthy/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { AccountComponent } from './account/account.component';
+import { MapComponent } from './map/map.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    AccountComponent
+    AccountComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     [BrowserAnimationsModule],
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCjU3_9873uVzSoXU50oV6pYyB1W0ExKBE'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
